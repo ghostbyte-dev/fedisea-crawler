39e07de35fa1c8828143250436ecf9a1b0748d53
Hiebeler <emanuel.hiebeler@gmail.com>
2026-02-24 12:04:35 +0100

    fetch well known

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
+use reqwest::Url;
+use serde::Deserialize;
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
+    let text = fetch_well_known("mastodon.social").await.expect("Failed to fetch");
+    println!("href: {}", text.links[0].href);
+    println!("rel: {}", text.links[0].rel);
+}
+
+#[derive(Deserialize)]
+struct WellKnown {
+    links: Vec<WellKnownElement>
+}
+
+#[derive(Deserialize)]
+struct WellKnownElement {
+    rel: String,
+    href: String
+}
+
+async fn fetch_well_known(instance: &str) -> Result<WellKnown, anyhow::Error> {
+    let url = format!(
+        "https://{}/.well-known/nodeinfo",
+        instance,
+    );
+    let url = Url::parse(&*url)?;
+
+    let res: WellKnown = reqwest::get(url).await?.json().await?;
+    Ok(res)
 }
