f661a24adccaec6833b9775c6fb6c4a2cd806077
Hiebeler <emanuel.hiebeler@gmail.com>
2026-02-24 12:20:38 +0100

    implement fetching well known, nodeinfo, peers

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,31 @@ use serde::Deserialize;
 #[tokio::main]
 async fn main() {
     println!("Hello, world!");
-    let text = fetch_well_known("mastodon.social").await.expect("Failed to fetch");
-    println!("href: {}", text.links[0].href);
-    println!("rel: {}", text.links[0].rel);
+    fetch_instance("mastodon.social").await;
+}
+
+async fn fetch_instance(instance: &str) {
+    let well_known = match fetch_well_known(instance).await {
+        Ok(well_known) => well_known,
+        Err(_) => return,
+    };
+
+    println!("Got instance: {}", instance);
+    println!("Got well known rel: {}", well_known.links[0].rel);
+
+    let nodeinfo = match fetch_nodeinfo(&*well_known.links[0].href).await {
+        Ok(nodeinfo) => nodeinfo,
+        Err(_) => return,
+    };
+
+    println!("Got software: {}", nodeinfo.software.name);
+
+    let peers = match fetch_peers(instance).await {
+        Ok(peers) => peers,
+        Err(_) => return,
+    };
+
+    println!("Got peers length: {}", peers.len());
 }
 
 #[derive(Deserialize)]
@@ -27,6 +49,32 @@ async fn fetch_well_known(instance: &str) -> Result<WellKnown, anyhow::Error> {
     );
     let url = Url::parse(&*url)?;
 
-    let res: WellKnown = reqwest::get(url).await?.json().await?;
+    let res: WellKnown = reqwest::get(url).await?.error_for_status()?.json().await?;
+    Ok(res)
+}
+
+async fn fetch_peers(instance: &str) -> Result<Vec<String>, anyhow::Error> {
+    let url = format!("https://{}/api/v1/instance/peers", instance);
+    let url = Url::parse(&*url)?;
+
+    let res: Vec<String> = reqwest::get(url).await?.error_for_status()?.json().await?;
+    Ok(res)
+}
+
+#[derive(Deserialize)]
+struct Nodeinfo {
+    software: Software
+}
+
+#[derive(Deserialize)]
+struct Software {
+    name: String,
+    version: String
+}
+
+async fn fetch_nodeinfo(url: &str) -> Result<Nodeinfo, anyhow::Error> {
+    let url = Url::parse(url)?;
+
+    let res: Nodeinfo = reqwest::get(url).await?.error_for_status()?.json().await?;
     Ok(res)
 }
