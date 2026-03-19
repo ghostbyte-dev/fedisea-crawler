85f1d914be1ea55525a035d539e3fe5adf24ec90
Emanuel Hiebeler <e12519646@student.tuwien.ac.at>
2026-03-19 23:53:02 +0100

    update user agent

--- src/consts.rs
+++ src/consts.rs
@@ -1,2 +1,2 @@
-pub const USER_AGENT: &str = "Fedisea (https://github.com/ghostbyte-dev/fedisea-crawler)";
+pub const USER_AGENT: &str = "FediSeaBot (https://github.com/ghostbyte-dev/fedisea-crawler)";
 pub const WORKERS: i8 = 4;
